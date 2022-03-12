8e82bc95cf53c1328dd537eca28bb9a555cf6d51
PlasTec <100198052+PlasTecc@users.noreply.github.com>
2022-03-12 15:26:08 +0300

    Update CPSTOOL.py

--- CPSTOOL.py
+++ CPSTOOL.py
@@ -13,6 +13,7 @@ class bcolors:
     OKBLUE = '\033[94m'
     OKCYAN = '\033[96m'
     OKGREEN = '\033[92m'
+    OKWHITE = '\033[97m'
     WARNING = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
@@ -23,28 +24,34 @@ class bcolors:
 appdata = os.getenv('APPDATA')
 
 try:
-    key = json.load(open(f"{appdata}\\key.json", "rb"))["key"]
+    selected_key = json.load(open(f"{appdata}\\data.json", "rb"))["key"]
+    classroom = json.load(open(f"{appdata}\\data.json", "rb"))["classroom"]
 except FileNotFoundError:
-    json_data = {"key": " "}
-    with open(f"{appdata}\\key.json", "w") as outfile:
+    json_data = {"key": "", "timetable": {}, "zoomlinks": {}, "classroom": ""}
+    with open(f"{appdata}\\data.json", "w") as outfile:
         json.dump(json_data, outfile)
-    key = json.load(open(f"{appdata}\\key.json", "rb"))["key"]
+    selected_key = json.load(open(f"{appdata}\\data.json", "rb"))["key"]
+    classroom = json.load(open(f"{appdata}\\data.json", "rb"))["classroom"]
 
 current_day = date.today().strftime("%A")
-api_key = "ApiKeyHere"
+api_key = "api_key_here"
 api_url = "https://api.jsonbin.io/v3/b/"
 api_header = {"X-Master-Key": api_key}
 
 
 def banner():
-    banner = f"""\033[97m
+    banner = f"""{bcolors.OKWHITE}
 █████████████████████████████████████████
 █─▄▄▄─█▄─▄▄─█─▄▄▄▄█─▄─▄─█─▄▄─█─▄▄─█▄─▄███
 █─███▀██─▄▄▄█▄▄▄▄─███─███─██─█─██─██─██▀█
-▀▄▄▄▄▄▀▄▄▄▀▀▀▄▄▄▄▄▀▀▄▄▄▀▀▄▄▄▄▀▄▄▄▄▀▄▄▄▄▄▀ PlasTec#5267 | V1.0 | {key}
+▀▄▄▄▄▄▀▄▄▄▀▀▀▄▄▄▄▄▀▀▄▄▄▀▀▄▄▄▄▀▄▄▄▄▀▄▄▄▄▄▀ PlasTec#5267 | V2.0 | {selected_key} | {classroom}
 """
     os.system('cls' if os.name == 'nt' else 'clear')
     print(banner)
+
+
+timetable = json.load(open(f"{appdata}\\data.json", "rb"))["timetable"]
+zoomlinks = json.load(open(f"{appdata}\\data.json", "rb"))["zoomlinks"]
 
 
 def mainMenu():
@@ -55,24 +62,20 @@ def mainMenu():
         print("2. CREATE TIMETABLE")
         print("3. EDIT KEY")
         print("4. VIEW CURRENT SELECTED TIMETABLE")
-        user_input = int(input("> "))
-        if -1 < user_input < 5:
-            if user_input == 0:
+        user_input = input("> ")
+        if 0 <= int(user_input) <= 4:
+            if int(user_input) == 0:
                 endMenu()
-            if user_input == 1:
-                joinmeetingMenu()
-            if user_input == 2:
-                createtimetableMenu()
-            if user_input == 3:
-                editkeyMenu()
-            if user_input == 4:
-                viewtimetable()
+            if int(user_input) == 1:
+                joinMenu()
+            if int(user_input) == 2:
+                createMenu()
+            if int(user_input) == 3:
+                keyMenu()
+            if int(user_input) == 4:
+                viewMenu()
         else:
-            print(f"{bcolors.FAIL}INVALID CHOICE!{bcolors.ENDC}")
-            if input(f"{bcolors.WARNING}WOULD YOU LIKE TO TRY AGAIN? (Y/N): {bcolors.ENDC}").upper() == "Y":
-                mainMenu()
-            else:
-                endMenu()
+            raise ValueError
     except ValueError:
         print(f"{bcolors.FAIL}INVALID INPUT!{bcolors.ENDC}")
         if input(f"{bcolors.WARNING}WOULD YOU LIKE TO TRY AGAIN? (Y/N): {bcolors.ENDC}").upper() == "Y":
@@ -81,76 +84,62 @@ def mainMenu():
             endMenu()
 
 
-def joinmeetingMenu():
+def joinMenu():
     banner()
-    if current_day != "Friday" and current_day != "Saturday":
-        if 25 >= len(key):
-            r = requests.get(api_url + key, json=None, headers=api_header)
-            if r.status_code == 200:
-                data = r.json()
-                timetable = data["record"]["timetable"][current_day]
-                zoomlinks = data["record"]["zoomlinks"]
-                for subject in timetable:
-                    print(f"{timetable.index(subject) + 1}.{subject}")
-                try:
-                    subject_choice = int(input("> "))
-                    if 0 < subject_choice <= len(timetable):
-                        subject_index = subject_choice - 1
-                        if len(zoomlinks[timetable[subject_index]]) > 29:
-                            webbrowser.open(
-                                zoomlinks[timetable[subject_index]])
-                            if input(f"{bcolors.WARNING}WOULD YOU LIKE TO GO BACK? (Y/N): {bcolors.ENDC}").upper() == "Y":
-                                mainMenu()
-                            else:
-                                joinmeetingMenu()
+    if selected_key:
+        if current_day != "Friday" and current_day != "Saturday":
+            currentday_timetable = timetable[current_day]
+            for subject in currentday_timetable:
+                print(f"{currentday_timetable.index(subject) + 1}. {subject}")
+            subject_input = input("> ")
+            try:
+                if int(subject_input) > 0:
+                    subject_index = int(subject_input) - 1
+                    zoomlink = zoomlinks[currentday_timetable[subject_index]]
+                    if len(zoomlink) >= 30:
+                        webbrowser.open(zoomlink)
+                        if input("WOULD YOU LIKE TO TRY AGAIN? (Y/N): ").upper() == "Y":
+                            joinMenu()
                         else:
-                            print(f"{bcolors.FAIL}CHECK WHATSAPP!{bcolors.ENDC}")
-                            if input(f"{bcolors.WARNING}WOULD YOU LIKE TO GO BACK? (Y/N): {bcolors.ENDC}").upper() == "Y":
-                                mainMenu()
-                            else:
-                                joinmeetingMenu()
-                    else:
-                        print(f"{bcolors.FAIL}INVALID CHOICE!{bcolors.ENDC}")
-                        if input(f"{bcolors.WARNING}WOULD YOU LIKE TO GO BACK? (Y/N): {bcolors.ENDC}").upper() == "Y":
                             mainMenu()
-                        else:
-                            joinmeetingMenu()
-                except ValueError:
-                    print(f"{bcolors.FAIL}INVALID INPUT!{bcolors.ENDC}")
-                    if input(f"{bcolors.WARNING}WOULD YOU LIKE TO GO BACK? (Y/N): {bcolors.ENDC}").upper() == "Y":
-                        mainMenu()
                     else:
-                        joinmeetingMenu()
-            else:
-                print(f"{bcolors.FAIL}INVALID KEY!{bcolors.ENDC}")
-                input(f"{bcolors.WARNING}PRESS ENTER TO GO BACK...{bcolors.ENDC}")
-                mainMenu()
+                        print(f"{bcolors.FAIL}CHECK WHATSAPP!{bcolors.ENDC}")
+                        if input(f"{bcolors.WARNING}WOULD YOU LIKE TO TRY AGAIN? (Y/N): {bcolors.ENDC}").upper() == "Y":
+                            joinMenu()
+                        else:
+                            mainMenu()
+                else:
+                    raise Exception
+            except Exception:
+                print(f"{bcolors.FAIL}INVALID CHOICE!{bcolors.ENDC}")
+                if input(f"{bcolors.WARNING}WOULD YOU LIKE TO TRY AGAIN? (Y/N): {bcolors.ENDC}").upper() == "Y":
+                    joinMenu()
+                else:
+                    mainMenu()
         else:
-            print(f"{bcolors.FAIL}INVALID KEY!{bcolors.ENDC}")
-            if input(f"{bcolors.WARNING}WOULD YOU LIKE TO GO BACK? (Y/N): {bcolors.ENDC}").upper() == "Y":
-                mainMenu()
-            else:
-                joinmeetingMenu()
-    else:
-        print(f"{bcolors.WARNING}THERE IS NO SCHOOL TODAY!{bcolors.ENDC}")
-        if input(f"{bcolors.WARNING}WOULD YOU LIKE TO GO BACK? (Y/N): {bcolors.ENDC}").upper() == "Y":
+            print(f"{bcolors.FAIL}THERE IS NO SCHOOL TODAY!{bcolors.ENDC}")
+            input(f"{bcolors.WARNING}PRESS ENTER TO GO...{bcolors.ENDC}")
             mainMenu()
-        else:
-            joinmeetingMenu()
+    else:
+        print(f"{bcolors.FAIL}PLEASE INPUT A KEY!{bcolors.ENDC}")
+        input(f"{bcolors.WARNING}PRESS ENTER TO GO BACK...{bcolors.ENDC}")
+        mainMenu()
 
 
-def createtimetableMenu():
+def createMenu():
+    banner()
     while True:
-        data = {"timetable": {"Sunday": [], "Monday": [], "Tuesday": [],
-                              "Wednesday": [], "Thursday": []}, "zoomlinks": {}}
-        for day in data["timetable"]:
+        new_classroom = input("CLASSROOM: ")
+        new_data = {"timetable": {"Sunday": [], "Monday": [], "Tuesday": [],
+                                  "Wednesday": [], "Thursday": []}, "zoomlinks": {}, "classroom": new_classroom}
+        for day in new_data["timetable"]:
             banner()
             print(f"{day}: ")
             count = 1
             while count <= 8:
                 subject = input(f"Period {count}: ").upper()
                 if subject:
-                    data["timetable"][day].append(subject)
+                    new_data["timetable"][day].append(subject)
                     count += 1
                 else:
                     count = 1
@@ -159,54 +148,56 @@ def createtimetableMenu():
         tabulate_header = ["", "1", "2", "3", "4", "5", "6", "7", "8"]
         tabulate_timetable = [[], [], [], [], []]
         index = 0
-        for day in data["timetable"]:
+        for day in new_data["timetable"]:
             tabulate_timetable[index].append(day)
-            for subject in data["timetable"][day]:
+            for subject in new_data["timetable"][day]:
                 tabulate_timetable[index].append(subject)
             index += 1
         print(tabulate(tabulate_timetable, headers=tabulate_header))
         if input(f"{bcolors.WARNING}DO YOU LIKE IT? (Y/N): {bcolors.ENDC}").upper() == "Y":
             break
         else:
-            if input(f"{bcolors.WARNING}DO YOU WANT TO GO BACK? (Y/N): {bcolors.ENDC}").upper() == "Y":
+            if input(f"{bcolors.WARNING}WOULD YOU LIKE TO TRY AGAIN? (Y/N): {bcolors.ENDC}").upper() == "N":
                 mainMenu()
 
     allsubjects = []
-    for day in data["timetable"]:
-        allsubjects += data["timetable"][day]
+    for day in new_data["timetable"]:
+        allsubjects += new_data["timetable"][day]
     allsubjects = sorted(list(dict.fromkeys(allsubjects)))
 
     while True:
         for subject in allsubjects:
             while True:
                 banner()
+                print(
+                    f"{bcolors.WARNING}LEAVE INPUT EMPTY FOR SUBJECTS LIKE GH!{bcolors.ENDC}")
                 zoom_code = input(f"{subject}: https://cpsbahrain.zoom.us/j/")
                 if len(zoom_code) >= 9 and zoom_code.isdecimal():
-                    data["zoomlinks"][subject] = f"https://cpsbahrain.zoom.us/j/{zoom_code}"
+                    new_data["zoomlinks"][subject] = f"https://cpsbahrain.zoom.us/j/{zoom_code}"
                     break
                 elif zoom_code == "":
-                    data["zoomlinks"][subject] = f"https://cpsbahrain.zoom.us/j/{zoom_code}"
+                    new_data["zoomlinks"][subject] = f"https://cpsbahrain.zoom.us/j/{zoom_code}"
                     break
                 else:
                     print(f"{bcolors.WARNING}INVALID CODE!{bcolors.ENDC}")
-                    input(f"{bcolors.FAIL}PRESS ENTER TO RETRY{bcolors.ENDC}")
+                    input(f"{bcolors.FAIL}PRESS ENTER TO RETRY...{bcolors.ENDC}")
 
         tabulate_zoomlink = []
 
-        for subject in data["zoomlinks"]:
-            tabulate_zoomlink.append([subject, data["zoomlinks"][subject]])
+        for subject in new_data["zoomlinks"]:
+            tabulate_zoomlink.append([subject, new_data["zoomlinks"][subject]])
 
         banner()
         print(tabulate(tabulate_zoomlink))
         if input(f"{bcolors.WARNING}DO YOU LIKE IT? (Y/N): {bcolors.ENDC}").upper() == "Y":
             break
         else:
-            if input(f"{bcolors.WARNING}DO YOU WANT TO GO BACK? (Y/N): {bcolors.ENDC}").upper() == "Y":
+            if input(f"{bcolors.WARNING}WOULD YOU LIKE TO TRY AGAIN? (Y/N): {bcolors.ENDC}").upper() == "N":
                 mainMenu()
 
     while True:
         api_header["Content-Type"] = "application/json"
-        r = requests.post(api_url, json=data, headers=api_header)
+        r = requests.post(api_url, json=new_data, headers=api_header)
         if r.status_code == 200:
             new_key = r.json()["metadata"]["id"]
             print(f"{bcolors.OKGREEN}Your key is: {new_key}{bcolors.ENDC}")
@@ -222,54 +213,78 @@ def createtimetableMenu():
     mainMenu()
 
 
-def editkeyMenu():
-    while True:
+def keyMenu():
+    try:
         banner()
-        new_key = input("KEY: ")
-        key_json = {"key": new_key}
+        key_input = input("KEY: ")
         if input(f"{bcolors.WARNING}ARE YOU SURE THIS IS THE CORRECT KEY? (Y/N): {bcolors.ENDC}").upper() == "Y":
-            break
-
-    with open(f"{appdata}\\key.json", "w") as outfile:
-        json.dump(key_json, outfile)
-    global key
-    key = new_key
-    mainMenu()
-
-
-def viewtimetable():
-    banner()
-    if 25 >= len(key):
-        r = requests.get(api_url + key, json=None, headers=api_header)
-        if r.status_code == 200:
-            data = r.json()
-            timetable = data["record"]["timetable"]
-            zoomlinks = data["record"]["zoomlinks"]
-            tabulate_header = ["", "1", "2", "3", "4", "5", "6", "7", "8"]
-            tabulate_timetable = [[], [], [], [], []]
-            index = 0
-            for day in timetable:
-                tabulate_timetable[index].append(day)
-                for subject in timetable[day]:
-                    tabulate_timetable[index].append(subject)
-                index += 1
-            print(tabulate(tabulate_timetable, headers=tabulate_header))
-
-            all_links = []
-
-            for name in zoomlinks:
-                all_links.append([name, zoomlinks[name]])
-
-            print(tabulate(all_links))
-
-            input(f"{bcolors.WARNING}PRESS ENTER TO GO BACK...{bcolors.ENDC}")
-            mainMenu()
+            if len(key_input) >= 24 and key_input.isalnum():
+                r = requests.get(api_url+key_input, json=None,
+                                 headers=api_header)
+                if r.status_code == 200:
+                    data = r.json()
+                    data_json = {
+                        "key": key_input,
+                        "timetable": data["record"]["timetable"],
+                        "zoomlinks": data["record"]["zoomlinks"],
+                        "classroom": data["record"]["classroom"]
+                    }
+                    with open(f"{appdata}\\data.json", "w") as outfile:
+                        json.dump(data_json, outfile)
+                    global selected_key
+                    global timetable
+                    global zoomlinks
+                    global classroom
+                    selected_key = key_input
+                    timetable = json.load(open(f"{appdata}\\data.json", "rb"))[
+                        "timetable"]
+                    zoomlinks = json.load(open(f"{appdata}\\data.json", "rb"))[
+                        "zoomlinks"]
+                    classroom = json.load(open(f"{appdata}\\data.json", "rb"))[
+                        "classroom"]
+                    print(f"{bcolors.OKGREEN}CHANGED KEY!{bcolors.ENDC}")
+                    input(
+                        f"{bcolors.WARNING}PRESS ENTER TO GO BACK...{bcolors.ENDC}")
+                    mainMenu()
+                else:
+                    raise Exception
+            else:
+                raise Exception
         else:
-            print(f"{bcolors.FAIL}INVALID KEY!{bcolors.ENDC}")
-            input(f"{bcolors.WARNING}PRESS ENTER TO GO BACK...{bcolors.ENDC}")
-            mainMenu()
-    else:
+            if input(f"{bcolors.WARNING}WOULD YOU LIKE TO TRY AGAIN? (Y/N): {bcolors.ENDC}").upper() == "Y":
+                keyMenu()
+            else:
+                mainMenu()
+    except Exception:
         print(f"{bcolors.FAIL}INVALID KEY!{bcolors.ENDC}")
+        if input(f"{bcolors.WARNING}WOULD YOU LIKE TO TRY AGAIN? (Y/N): {bcolors.ENDC}").upper() == "Y":
+            keyMenu()
+        else:
+            mainMenu()
+
+
+def viewMenu():
+    banner()
+    if selected_key:
+        tabulate_header = ["", "1", "2", "3", "4", "5", "6", "7", "8"]
+        tabulate_timetable = [[], [], [], [], []]
+        index = 0
+        for day in timetable:
+            tabulate_timetable[index].append(day)
+            for subject in timetable[day]:
+                tabulate_timetable[index].append(subject)
+            index += 1
+        print(tabulate(tabulate_timetable, headers=tabulate_header))
+
+        all_links = []
+        for name in zoomlinks:
+            all_links.append([name+":", zoomlinks[name]])
+        print(tabulate(all_links))
+
+        input(f"{bcolors.WARNING}PRESS ENTER TO GO BACK...{bcolors.ENDC}")
+        mainMenu()
+    else:
+        print(f"{bcolors.FAIL}PLEASE INPUT A KEY!{bcolors.ENDC}")
         input(f"{bcolors.WARNING}PRESS ENTER TO GO BACK...{bcolors.ENDC}")
         mainMenu()
 
